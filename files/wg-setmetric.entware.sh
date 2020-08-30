156c0d331cae307b920210576dc249d5ebfcfa88
Ivan Andreev <ivandeex@gmail.com>
2020-08-31 00:40:39 +0300

    implement optional route priority. also rename default -> d()

--- files/wg-setmetric.entware.sh
+++ files/wg-setmetric.entware.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/opt/bin/bash
 #set -x
 set -euo pipefail
 
@@ -16,21 +16,21 @@ fi
 
 for proto in -4 -6; do
     ## TODO handle routing tables and policy routing
-    /sbin/ip -o "$proto" route show dev "$dev" | \
+    /opt/sbin/ip -o "$proto" route show dev "$dev" | \
     while read route; do
         if [ "$route" = "${route/metric/}" ]; then
             # shellcheck disable=SC2086
-            /sbin/ip "$proto" route del $route dev "$dev"
+            /opt/sbin/ip "$proto" route del $route dev "$dev"
             # shellcheck disable=SC2086
-            /sbin/ip "$proto" route add $route metric "$metric" dev "$dev"
+            /opt/sbin/ip "$proto" route add $route metric "$metric" dev "$dev"
             continue
         fi
         route_new=$(echo "$route" | sed -r -e "s/metric [0-9]+/metric $metric/")
         if [ "$route" != "$route_new" ]; then
             # shellcheck disable=SC2086
-            /sbin/ip "$proto" route del $route dev "$dev"
+            /opt/sbin/ip "$proto" route del $route dev "$dev"
             # shellcheck disable=SC2086
-            /sbin/ip "$proto" route add $route_new dev "$dev"
+            /opt/sbin/ip "$proto" route add $route_new dev "$dev"
         fi
     done
 done
